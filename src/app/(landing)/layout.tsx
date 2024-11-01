c0af14f187b47c40510832c7d955c0ffae93fda3
Tkhangds <gunnyvippro111@gmail.com>
2024-11-01 23:43:58 +0700

    feat: add landing page components and layout structure

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,14 @@
+import { Prisma } from '@prisma/client'
+import React from 'react'
+
+const LandingLayout = (props: {
+    children: React.ReactNode
+}) => {
+  return (
+    <div>
+      {props.children}
+    </div>
+  )
+}
+
+export default LandingLayout
