dcc7a41c926eb72ad6ca704e9d1faa0858196509
huytt268 <tuanhuytruong35@gmail.com>
2024-11-24 22:10:59 +0700

    feat: fetch dynamic data for admin order page

--- src/types/order.d.ts
+++ src/types/order.d.ts
@@ -7,17 +7,18 @@ type Order = {
   scheduledEndTime: Date;
   helperRating?: number | null;
   totalPrice: number;
-  status: 'Pending' | 'In Progress' | 'Cancelled' | 'Completed';
+  status: "Pending" | "In Progress" | "Cancelled" | "Completed";
 };
 
 type OrderRowProps = {
-  id: string;
-  customerName: string;
-  helperName: string;
-  location: string;
-  scheduledStartTime: Date;
-  scheduledEndTime: Date;
-  helperRating?: number | null;
-  totalPrice: number;
-  status: 'Pending' | 'In Progress' | 'Cancelled' | 'Completed';
+  // id: string;
+  // customerName: string;
+  // helperName: string;
+  // location: string;
+  // scheduledStartTime: Date;
+  // scheduledEndTime: Date;
+  // helperRating?: number | null;
+  // totalPrice: number;
+  // status: 'Pending' | 'In Progress' | 'Cancelled' | 'Completed';
+  booking: Booking;
 };
