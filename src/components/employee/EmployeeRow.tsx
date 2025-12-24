fd8a0581d01c61756f6ff9beedc386eb03393ee9
khoavale-uno <khoavale.kms@uno.bank>
2025-12-24 14:07:16 +0700

    Hide employee info

--- src/components/employee/EmployeeRow.tsx
+++ src/components/employee/EmployeeRow.tsx
@@ -43,7 +43,7 @@ const EmployeeRow: React.FC<EmployeeRowProps> = ({
   };
 
   return (
-    <div onClick={() => router.push(`${pathName}/${id}`)} className="flex flex-wrap gap-3 w-full border-b border-gray-200 bg-white hover:bg-[#f4f7ff] h-auto items-start md:items-center p-2.5 cursor-pointer">
+    <div className="flex flex-wrap gap-3 w-full border-b border-gray-200 bg-white hover:bg-[#f4f7ff] h-auto items-start md:items-center p-2.5 cursor-pointer">
       <div className="w-full md:w-[10%] flex items-center justify-start md:py-6 mb-2 md:mb-0">
         <div className="text-sm text-[#202224] font-semibold break-all line-clamp-2">
           <span className="md:hidden font-bold">ID: </span>
